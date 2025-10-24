26d332a81f68b76732c84e4b6e90012bc4c4b075
rp9876 <212578794+rp9876@users.noreply.github.com>
2025-10-24 17:13:52 +0100

    new python exercise files added

---
+++ RuqsToolKit/modules/chunker.py
@@ -0,0 +1,6 @@
+#for python exercise1, code to break up sequence in chunks of a given size
+
+def chunker(sequence, chunk_size):
+    return[sequence[i:i + chunk_size]]
+
+
--- RuqsToolKit/modules/slice_string.py
+++ RuqsToolKit/modules/slice_string.py
@@ -4,23 +4,35 @@
 #define new function stringslicer, which needs the following parameters: a piece of string (text), a start position (s1) and an end position (e1) or more
 #the function slices the text into 2 depending on the input integers and returns the 2 slices separated by a space
 def stringslicer (text, s1, e1, s2, e2):
-    slice1 = text[s1:e1]
-    slice2 = text[s2:e2]
+    slice1 = text[s1:e1+1]
+    slice2 = text[s2:e2+1]
     return slice1 + " " + slice2
 
 #the text is give, the result variable is defined using the function and inputs and is finally printed
-text = "TheUniversityOfManchester"
-result = stringslicer(text, 3, 12, 15, 25)
-print(result)
+#text = "TheUniversityOfManchester"
+#result = stringslicer(text, 3, 12, 15, 25)
+#print(result)
 
 #OR
 
-text = input("enter your text ")
-s1 = input("enter your start position ")
-e1 = input("enter your end position ")
+#text = str(input("enter your text "))
+#s1 = int(input("enter your start position "))
+#e1 = int(input("enter your end position "))
 
-s2 = input("enter your start position ")
-e2 = input("enter your end position ")
+#s2 = int(input("enter your start position "))
+#e2 = int(input("enter your end position "))
 
-result = stringslicer(text, 3, 12, 15, 25)
-print("Your sliced string is: ", result)
+#result = stringslicer(text, s1, e1, s2, e2)
+#print("Your sliced string is: ", result)
+
+
+
+#Challenge: what is the output given the following string and number pairs 137,143 and 274,280?
+
+
+sequence = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTGAT"
+result = stringslicer(sequence, 137, 143, 274, 280)
+print("Your sliced sequence is: ", result)
+
+#answer from terminal: Your sliced sequence is:  GAAGAAG GAAGAAA
+
